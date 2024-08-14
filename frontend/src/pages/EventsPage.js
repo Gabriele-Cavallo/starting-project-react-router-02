4f7e084eb550189f0828fa1093f5c006733c0925
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-14 12:51:44 +0200

    feat: add useLoaderData

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,32 +1,14 @@
-import { Link } from "react-router-dom";
+import EventsList from '../components/EventsList';
+import { useLoaderData } from 'react-router-dom';
 
-const DUMMY_EVENTS = [
-    {
-        id: 'e1',
-        title: 'event-1',
-        place: 'place-1',
-        date: 'today'
-    },
-    {
-        id: 'e2',
-        title: 'event-2',
-        place: 'place-2',
-        date: 'today'
-    },
-    {
-        id: 'e3',
-        title: 'event-3',
-        place: 'place-3',
-        date: 'today'
-    },
-];
-
-export default function EventsPage(){
-    return (
-        <>
-            <ul>
-                {DUMMY_EVENTS.map(event => <li key={event.id}><Link to={`/events/${event.id}`}>{event.title}</Link></li>)}
-            </ul>
-        </>
-    )
+function EventsPage() {
+    const events = useLoaderData();
+    
+  return (
+    <>
+       <EventsList events={events} />
+    </>
+  );
 }
+
+export default EventsPage;
